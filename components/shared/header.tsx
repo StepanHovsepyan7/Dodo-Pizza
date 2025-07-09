881b84ff6e83e5557b0cf2fbf6c6ea8975edbf08
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-07-09 18:42:44 +0400

    finished the search part of the products

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import Image from "next/image"
 import { Button } from "../ui"
 import { Container } from "./container"
 import { ArrowRight, ShoppingCart, User } from "lucide-react"
+import Link from "next/link"
+import { SearchInput } from "./search-input"
 
 interface Props {
     className?: string
@@ -13,17 +15,24 @@ export const Header: React.FC<Props> = ({ className }) => {
     return (
         <header className={cn('border border-b', className)}>
             <Container className="flex items-center justify-between py-8">
-                <div className="flex items-center gap-4">
-                    <Image src="/logo.png" alt="Logo" width={35} height={35} />
-                    <div>
-                        <h1 className="text-2xl uppercase font-black">NEXT PIZZA</h1>
-                        <p className="text-sm text-[#7B7B7B] leading-3">вкусней уже не будет</p>
+                <Link href="/">
+                    <div className="flex items-center gap-4">
+                        <Image src="/logo.png" alt="Logo" width={35} height={35} />
+                        <div>
+                            <h1 className="text-2xl uppercase font-black">NEXT PIZZA</h1>
+                            <p className="text-sm text-[#7B7B7B] leading-3">вкусней уже не будет</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
+
+
+            <div className="mx-10 flex-1">
+                 <SearchInput />
+            </div>
 
 
                 <div className="flex items-center gap-1">
-                    <Button  className="flex  items-center gap-3 text-[#FE5F00] border border-[#FE5F00] rounded-[15px]" variant="outline"><User size={16} />Войти</Button>
+                    <Button className="flex  items-center gap-3 text-[#FE5F00] border border-[#FE5F00] rounded-[15px]" variant="outline"><User size={16} />Войти</Button>
                     <div>
                         <Button className="group relative bg-[#FE5F00] shopBtn" >
                             <b>520 ₽</b>
